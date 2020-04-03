930169680fb2f4c1ae94edcddc704d9307d38b82
DeAndre Boston <dboston3@yahoo.com>
2020-04-03 12:43:24 -0500

    Sort & Filter Data

--- scripts/FriendlyEats.Data.js
+++ scripts/FriendlyEats.Data.js
@@ -32,7 +32,6 @@ FriendlyEats.prototype.getAllRestaurants = function(renderer) {
     .collection('restaurants')
     .orderBy('avgRating', 'desc')
     .limit(50);
-  console.log(query);
   this.getDocumentsInQuery(query, renderer);
 };
 
@@ -68,6 +67,27 @@ FriendlyEats.prototype.getFilteredRestaurants = function(filters, renderer) {
   /*
     TODO: Retrieve filtered list of restaurants
   */
+  var query = firebase.firestore().collection('restaurants');
+
+  if (filters.category !== 'Any') {
+    query = query.where('category', '==', filters.category);
+  }
+
+  if (filters.city !== 'Any') {
+    query = query.where('city', '==', filters.city);
+  }
+
+  if (filters.price !== 'Any') {
+    query = query.where('price', '==', filters.price.length);
+  }
+
+  if (filters.sort === 'Rating') {
+    query = query.orderBy('avgRating', 'desc');
+  } else if (filters.sort === 'Reviews') {
+    query = query.orderBy('numRatings', 'desc');
+  }
+
+  this.getDocumentsInQuery(query, renderer);
 };
 
 FriendlyEats.prototype.addRating = function(restaurantID, rating) {
